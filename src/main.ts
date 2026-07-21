5b7afde40f6a26e087a73b11e43830aac9cde11f
HRY21 <70255962+Carrie124@users.noreply.github.com>
2026-07-21 16:35:55 +1000

    feat(swagger): configure Swagger docs with Bearer auth support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Cross-Origin Resource Sharing
+  // Tell NestJS "Allow other sources to access my interface"
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Order Management API')
+    .setDescription('Order Management system Interface document')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 5001);
   console.log('Application is running on: http://localhost:5001');
 }
